300783fa2926affe5ea62d09b89d154815f1b021
Kai <kailukowiak@gmail.com>
2018-03-08 21:27:02 -0700

    Posted question

--- Assignment3/Problem1/app.R
+++ Assignment3/Problem1/app.R
@@ -1,3 +1,13 @@
+# Question 1:
+# As a researcher, you frequently compare mortality rates from particular causes across
+# different States. You need a visualization that will let you see (for 2010 only) the crude
+# mortality rate, across all States, from one cause (for example, Neoplasms, which are
+# effectively cancers). Create a visualization that allows you to rank States by crude mortality
+# for each cause of death.
+
+
+
+
 # Global variables can go here
 library(tidyquant)
 library(tidyr)
